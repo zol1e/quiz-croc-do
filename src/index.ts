1b4f33dbfbbc449bceb5c653981b4e2c0f051d9c
zol1e <zoltan.szeg@gmail.com>
2025-12-28 23:54:45 +0100

    implement game ws

--- src/index.ts
+++ src/index.ts
@@ -1,64 +1,126 @@
 import { DurableObject } from "cloudflare:workers";
 
-/**
- * Welcome to Cloudflare Workers! This is your first Durable Objects application.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your Durable Object in action
- * - Run `npm run deploy` to publish your application
- *
- * Bind resources to your worker in `wrangler.jsonc`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/durable-objects
- */
 
-/** A Durable Object's behavior is defined in an exported Javascript class */
-export class MyDurableObject extends DurableObject<Env> {
-	/**
-	 * The constructor is invoked once upon creation of the Durable Object, i.e. the first call to
-	 * 	`DurableObjectStub::get` for a given identifier (no-op constructors can be omitted)
-	 *
-	 * @param ctx - The interface for interacting with Durable Object state
-	 * @param env - The interface to reference bindings declared in wrangler.jsonc
-	 */
+export class QuizCrocGameDO extends DurableObject<Env> {
+
+	sessions: Map<WebSocket, { [key: string]: string }>;
+
 	constructor(ctx: DurableObjectState, env: Env) {
 		super(ctx, env);
+		this.sessions = new Map();
+
+		// As part of constructing the Durable Object,
+		// we wake up any hibernating WebSockets and
+		// place them back in the `sessions` map.
+
+		// Get all WebSocket connections from the DO
+		this.ctx.getWebSockets().forEach((ws) => {
+			let attachment = ws.deserializeAttachment();
+			if (attachment) {
+				// If we previously attached state to our WebSocket,
+				// let's add it to `sessions` map to restore the state of the connection.
+				this.sessions.set(ws, { ...attachment });
+			}
+		});
+
+		// Sets an application level auto response that does not wake hibernated WebSockets.
+		this.ctx.setWebSocketAutoResponse(new WebSocketRequestResponsePair('ping', 'pong'));
 	}
 
-	/**
-	 * The Durable Object exposes an RPC method sayHello which will be invoked when a Durable
-	 *  Object instance receives a request from a Worker via the same method invocation on the stub
-	 *
-	 * @param name - The name provided to a Durable Object instance from a Worker
-	 * @returns The greeting to be sent back to the Worker
-	 */
+	async fetch(request: Request): Promise<Response> {
+		// Creates two ends of a WebSocket connection.
+		const webSocketPair = new WebSocketPair();
+		const [client, server] = Object.values(webSocketPair);
+
+		// Calling `acceptWebSocket()` informs the runtime that this WebSocket is to begin terminating
+		// request within the Durable Object. It has the effect of "accepting" the connection,
+		// and allowing the WebSocket to send and receive messages.
+		this.ctx.acceptWebSocket(server);
+
+		// Generate a random UUID for the session.
+		const id = crypto.randomUUID();
+
+		// Attach the session ID to the WebSocket connection and serialize it.
+		// This is necessary to restore the state of the connection when the Durable Object wakes up.
+		server.serializeAttachment({ id });
+
+		// Add the WebSocket connection to the map of active sessions.
+		this.sessions.set(server, { id });
+
+		return new Response(null, {
+			status: 101,
+			webSocket: client,
+		});
+	}
+
+	async webSocketMessage(ws: WebSocket, message: ArrayBuffer | string) {
+		// Get the session associated with the WebSocket connection.
+		const session = this.sessions.get(ws)!;
+
+		// Upon receiving a message from the client, the server replies with the same message, the session ID of the connection,
+		// and the total number of connections with the "[Durable Object]: " prefix
+		ws.send(`[Durable Object] message: ${message}, from: ${session.id}, to: the initiating client. Total connections: ${this.sessions.size}`);
+
+		// Send a message to all WebSocket connections, loop over all the connected WebSockets.
+		this.sessions.forEach((attachment, connectedWs) => {
+			connectedWs.send(`[Durable Object] message: ${message}, from: ${session.id}, to: all clients. Total connections: ${this.sessions.size}`);
+		});
+
+		// Send a message to all WebSocket connections except the connection (ws),
+		// loop over all the connected WebSockets and filter out the connection (ws).
+		this.sessions.forEach((attachment, connectedWs) => {
+			if (connectedWs !== ws) {
+				connectedWs.send(`[Durable Object] message: ${message}, from: ${session.id}, to: all clients except the initiating client. Total connections: ${this.sessions.size}`);
+			}
+		});
+	}
+
+	async webSocketClose(ws: WebSocket, code: number, reason: string, wasClean: boolean) {
+		// If the client closes the connection, the runtime will invoke the webSocketClose() handler.
+		this.sessions.delete(ws);
+		ws.close(code, 'Durable Object is closing WebSocket');
+	}
+
 	async sayHello(name: string): Promise<string> {
 		return `Hello, ${name}!`;
 	}
 }
 
 export default {
-	/**
-	 * This is the standard fetch handler for a Cloudflare Worker
-	 *
-	 * @param request - The request submitted to the Worker from the client
-	 * @param env - The interface to reference bindings declared in wrangler.jsonc
-	 * @param ctx - The execution context of the Worker
-	 * @returns The response to be sent back to the client
-	 */
-	async fetch(request, env, ctx): Promise<Response> {
-		// Create a stub to open a communication channel with the Durable Object
-		// instance named "foo".
-		//
-		// Requests from all Workers to the Durable Object instance named "foo"
-		// will go to a single remote Durable Object instance.
-		const stub = env.MY_DURABLE_OBJECT.getByName("foo");
+	
+	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
+		if (request.url.endsWith('/game-ws')) {
+		// Expect to receive a WebSocket Upgrade request.
+		// If there is one, accept the request and return a WebSocket Response.
+		const upgradeHeader = request.headers.get('Upgrade');
+		if (!upgradeHeader || upgradeHeader !== 'websocket') {
+			return new Response('Worker expected Upgrade: websocket', {
+			status: 426,
+			});
+		}
 
-		// Call the `sayHello()` RPC method on the stub to invoke the method on
-		// the remote Durable Object instance.
-		const greeting = await stub.sayHello("world");
+		if (request.method !== 'GET') {
+			return new Response('Worker expected GET method', {
+			status: 400,
+			});
+		}
 
-		return new Response(greeting);
+		// Since we are hard coding the Durable Object ID by providing the constant name 'foo',
+		// all requests to this Worker will be sent to the same Durable Object instance.
+		let stub = env.QUIZ_CROC_GAME_DO.getByName("foo");
+
+		return stub.fetch(request);
+		}
+
+		return new Response(
+			`Supported endpoints: /game-ws: Expects a WebSocket upgrade request`,
+			{
+				status: 200,
+				headers: {
+				'Content-Type': 'text/plain',
+				},
+			}
+		);
 	},
+
 } satisfies ExportedHandler<Env>;
